2eeb3b31d579f5ee52149b89ad4f838009357284
Philip Park <philippark3602@gmail.com>
2021-02-13 00:57:59 +0000

    Added Photography Page

---
+++ src/pages/photography/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../../components/Page";
+import Header from "../../components/Header";
+
+function PhotographyPage() {
+  return (
+    <Page>
+      <Header name="Photography" tagline="All my best photos" />
+    </Page>
+  );
+}
+
+export default PhotographyPage;
